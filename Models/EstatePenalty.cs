e4335ee27777ca1d36455cc6d772e56bc1d82fee
22123521 <22123521@ITWINLAB-8>
2024-07-26 15:27:09 +0000

    estatepenalty

---
+++ Models/EstatePenalty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace eThekwiniEstate.Models
+{
+    public class EstatePenalty
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PenaltyID { get; set; }
+        public int OwnerId { get; set; }
+        public virtual Owner Onwer { get; set; }
+        public int AreaCode { get; set; }
+        public virtual Area Area { get; set;}
+        public int ViolationCode { get; set; }
+        public double TPenaltyCost { get; set; }
+    }
+}
